67aa32f620a32e90d6a2413b235a7c7c17d95c39
ThierryRakotomanana <ThierryRktm@gmail.com>
2026-05-06 20:50:38 +0300

    fix: delete it

--- src/hooks/useTheme.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import { ThemeProviderContext } from '../const/theme'
-
-export const useTheme = () => {
-    const context = useContext(ThemeProviderContext)
-    return context
-}
